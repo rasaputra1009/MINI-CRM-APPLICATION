6950dba4442f379fae52ad54027d249e38eac8a6
Rohan Kamat <rohan.ka@media.net>
2018-10-24 12:48:55 +0530

    Use connected-react-router instead of react-router-redux

--- resources/src/commons/app/reducers.js
+++ resources/src/commons/app/reducers.js
@@ -2,45 +2,17 @@
  * Combine all reducers in this file and export the combined reducers.
  */
 
-import { fromJS } from 'immutable';
 import { combineReducers } from 'redux-immutable';
-import { LOCATION_CHANGE } from 'react-router-redux';
-
-
-/*
- * routeReducer
- *
- * The reducer merges route location changes into our immutable state.
- * The change is necessitated by moving to react-router-redux@5
- *
- */
-
-// Initial routing state
-const routeInitialState = fromJS({
-  location: null,
-});
-
-/**
- * Merge route into the global application state
- */
-function routeReducer(state = routeInitialState, action) {
-  switch (action.type) {
-    /* istanbul ignore next */
-    case LOCATION_CHANGE:
-      return state.merge({
-        location: action.payload,
-      });
-    default:
-      return state;
-  }
-}
+import { connectRouter } from 'connected-react-router/immutable';
+import { isEmpty, identity } from 'ramda';
 
 /**
  * Creates the main reducer with the dynamically injected ones
  */
-export default function createReducer(injectedReducers) {
-  return combineReducers({
-    route: routeReducer,
+export default function createReducer(history, injectedReducers = {}) {
+  const rootReducers = isEmpty(injectedReducers) ? identity : combineReducers({
     ...injectedReducers,
   });
+
+  return connectRouter(history)(rootReducers);
 }
--- resources/src/example-app/app/app.js
+++ resources/src/example-app/app/app.js
@@ -3,21 +3,22 @@ import 'babel-polyfill'; // eslint-disable-line
 // Import all the third party stuff
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Immutable from 'immutable';
 import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
-import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter } from 'connected-react-router/immutable';
+import { createBrowserHistory } from 'history';
 import 'sanitize.css/sanitize.css';
 
 // Import root app
-import App from 'containers/App'; // eslint-disable-line 
+import App from 'containers/App'; // eslint-disable-line
 
 // Load the favicon, the manifest.json file and the .htaccess file
 
 import configureStore from './configureStore';
 
 // Create redux store with history
-const initialState = {};
-const history = createHistory();
+const initialState = Immutable.Map();
+const history = createBrowserHistory();
 const store = configureStore(initialState, history);
 const MOUNT_NODE = document.getElementById('app');
 
