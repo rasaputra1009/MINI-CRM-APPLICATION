616088e7dab7ecb618108e72acb912807647fb54
Rohan Kamat <rohan.ka@media.net>
2018-10-24 13:04:14 +0530

    Upgrade to laravel 5.6

--- app/Modules/Example/Views/index.blade.php
+++ app/Modules/Example/Views/index.blade.php
@@ -5,6 +5,7 @@
 </head>
 <body>
     <div id='app'></div>
-<script src="{{normalize_chunks('/build/example-app/main.js')}}"></script>
+    <script src="{{normalize_chunks('/build/example-app/vendors~main.chunk.js')}}"></script>
+    <script src="{{normalize_chunks('/build/example-app/main.js')}}"></script>
 </body>
 </html>
